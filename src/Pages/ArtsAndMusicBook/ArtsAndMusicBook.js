3362a4d9c02b2191978f308bf5662c8c4302930f
Al Amin <alamin931761@gmail.com>
2022-11-21 22:30:16 +0600

    arts and music

--- src/Pages/ArtsAndMusicBook/ArtsAndMusicBook.js
+++ src/Pages/ArtsAndMusicBook/ArtsAndMusicBook.js
@@ -6,20 +6,22 @@ const ArtsAndMusicBook = ({ data }) => {
 
     const navigate = useNavigate();
     const navigateToDetails = (id) => {
-        navigate(`/bookDetails/${id}`)
+        navigate(`/bookDetails/${id}`);
     }
     return (
-        <div className="card w-[450px] bg-white shadow-2xl">
-            <figure><img className='h-[450px] w-[450px]' src={image} alt={name} /></figure>
-            <div className="card-body">
-                <h2 className="text-2xl font-bold">{name}</h2>
-                <p className='text-xl'>{author}</p>
-                <h2 className='text-2xl font-bold'>${price}</h2>
-                <div className="card-actions justify-end">
-                    <button onClick={() => navigateToDetails(_id)} className="btn btn-primary">Details</button>
+        <section className='flex justify-center'>
+            <div className="card w-[450px] bg-white shadow-2xl">
+                <figure><img className='h-[450px] w-[450px]' src={image} alt={name} /></figure>
+                <div className="card-body">
+                    <h2 className="text-2xl font-bold">{name}</h2>
+                    <p className='text-xl'>{author}</p>
+                    <h2 className='text-2xl font-bold'>${price}</h2>
+                    <div className="card-actions justify-end">
+                        <button onClick={() => navigateToDetails(_id)} className="btn btn-primary">Details</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
